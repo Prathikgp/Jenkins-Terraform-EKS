f7473c5a370d1d45bc6dcd15555795ee7d9a34fc
Prathikgp <prathikgp.1rn17me082@gmail.com>
2026-05-18 17:48:54 +0530

    Change instance_types to a list format

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -35,7 +35,7 @@ module "eks" {
                 max_size     = 3
                 min_size     = 1
 
-                instance_types = "t3.large"
+                instance_types = ["t3.large"]
                 capacity_type = "SPOT"
 
                 tags = {
